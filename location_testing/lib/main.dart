b30d7b5f68b8f4d44e5d6009d08844a1c56c7b1a
Pat <stumbaup@oregonstate.edu>
2021-03-05 12:27:07 -0800

    adding in firestore

--- location_testing/lib/main.dart
+++ location_testing/lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'screens/share_location_screen.dart';
 import 'screens/camera_screen.dart';
 import 'screens/remote_data_Screen.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
